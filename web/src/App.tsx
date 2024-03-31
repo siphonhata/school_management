3300628345024a34e1a131e65d231a4ece7ccea2
Dineo Mathibela <dineomathibela161@gmail.com>
2024-03-31 22:48:15 +0200

    init : react + tailwindcss

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-red-200">
+      <p className='text-5xl font-bold'>Testing tailwind</p>
+    </div>
+  );
+}
+
+export default App;
