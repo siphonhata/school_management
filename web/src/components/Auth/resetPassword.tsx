ecb12c3a393bab54c9b48fcedf2167a9e9dbb70d
Dineo Mathibela <dineomathibela161@gmail.com>
2024-04-01 16:40:19 +0200

    trial: reset component colors

--- web/src/components/Auth/resetPassword.tsx
+++ web/src/components/Auth/resetPassword.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 export const ResetPasswordCard = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-gray-100 shadow-md rounded px-10 py-8 mb-4 w-96">
+    <div className="flex justify-center items-center h-screen bg-green-700 text-white">
+      <div className="opacity-90 shadow-md rounded px-10 py-8 mb-4 w-96">
         <h2 className="text-2xl font-bold mb-6 text-center">Reset Password</h2>
         <div className="mb-2">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="newPassword">
+          <label className="block  text-sm font-bold mb-2" htmlFor="newPassword">
             New Password
           </label>
           <input
@@ -18,7 +18,7 @@ export const ResetPasswordCard = () => {
           />
         </div>
         <div className="mb-2">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
+          <label className="block  text-sm font-bold mb-2" htmlFor="confirmPassword">
             Confirm Password
           </label>
           <input
@@ -31,7 +31,7 @@ export const ResetPasswordCard = () => {
       <Link to={'/'}><p className='underline text-xs cursor-pointer'>Back to Login</p></Link>
         <div className="flex items-center justify-center mt-4">
           <button
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+            className="bg-green-900 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
             type="button"
           >
             Reset Password
